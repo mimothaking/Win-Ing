f1faa6cbdb50a3d1207fbfa99b2522e53cb814d1
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-02-28 12:03:34 +0100

    testing

--- wim-landing/src/App.js
+++ wim-landing/src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
-import '/uikit/js/uikit.min.js';
-import '/uikit/js/uikit-icons.min.js';
-import '/uikit/css/uikit.min.css';
+import './uikit/css/uikit.min.css';
+import './uikit/js/uikit.min.js';
+import './uikit/js/uikit-icons.min.js';
+
 
 
 
 function App() {
   return (
     <div className="App">
-    <p>  salam ! </p>
+  <nav class="uk-navbar-container uk-margin" uk-navbar="mode: click">
+    <div class="uk-navbar-left">
+
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="#">Active</a></li>
+            <li>
+                <a href="#">Parent</a>
+                <div class="uk-navbar-dropdown">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li class="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="#">Item</a></li>
+        </ul>
+
+    </div>
+</nav>
     </div>
   );
 }
